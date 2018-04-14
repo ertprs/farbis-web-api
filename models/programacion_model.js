5e1827e3dbe8a079c355dbcc24f8b9826de98847
Juan Alberto Carlos Vera <jcarlosverase@Juans-MacBook-Pro.local>
2018-04-13 19:27:30 -0500

    subida 13abr 1700

--- models/programacion_model.js
+++ models/programacion_model.js
@@ -121,7 +121,6 @@ module.exports = {
             var data = null;
             var msg = '';
             var id = '';
-        cnx.query(stmt, [ programaciones ], function(err, rows, fields) {
             console.log(err);
             console.log(rows);
             console.log(fields);
