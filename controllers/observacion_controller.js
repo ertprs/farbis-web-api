e45d619de69ba701934d8f353c4802d4c9d97d0c
Juan Carlos <jcarlosverase@gmail.com>
2019-10-15 00:52:01 -0500

    listar observaciones

--- controllers/observacion_controller.js
+++ controllers/observacion_controller.js
@@ -15,7 +15,7 @@ module.exports = {
 
         var id_programacion = req.body.id_programacion;
 
-        observacion_model.lista_por_programacion(id_programacion, function(msg, data){
+        observacion_model.lista_por_programacion(id_programacion, null, function(msg, data){
 
             var response = {
                 'ws_code' : '0',
